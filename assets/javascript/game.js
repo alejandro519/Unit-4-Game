565d992568c7d7a53db86b57c19387d5d0c5b436
Real Ghost <alejandro519@gmail.com>
2019-09-04 23:47:06 -0700

    Made additional styling edits

---
+++ assets/javascript/game.js
@@ -0,0 +1,12 @@
+//identify variable used for the game
+// var randomNum = 0;
+// var winsCounter = 0;
+// var lossesCounter = 0;
+// var scoreCounter = 0;
+// var scoreSum = 0;
+
+$("#crystals1").on("click", function(){
+    alert("test");
+
+})
+  
